ad533ef8a347a0a445241474267df8f447737d2e
Daniel Wilson <68431297+wanieldilson@users.noreply.github.com>
2021-07-02 14:14:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ mkdir -p tmp
 curl -L https://github.com/prometheus/blackbox_exporter/releases/download/v0.19.0/blackbox_exporter-0.19.0.linux-amd64.tar.gz --output ./tmp/bb.tar.gz
 tar -xzvf tmp/bb.tar.gz -C ./tmp
 mkdir -p build
-mv ./tmp/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter ./build/
+mv ./tmp/blackbox_exporter-0.19.0.linux-amd64/blackbox_exporter ./build
 
 docker build -t app-reachability .
